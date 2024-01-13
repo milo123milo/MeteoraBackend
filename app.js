a538271e6bd73e811277b95ede422b4635838b18
milo123milo <kosmonauti220@gmail.com>
2024-01-13 21:36:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ console.log(`Server is running on port ${PORT}`);
 
 app.use(cors());
 function rawBody(req, res, next) {
-  req.setEncoding('utf8');
+  //req.setEncoding('utf8');
   req.rawBody = '';
   req.on('data', function(chunk) {
     req.rawBody += chunk;
